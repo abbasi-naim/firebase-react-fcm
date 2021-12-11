87df59e16c519b1105f53321080991e230daffba
abbasi-naim <abbasi.naim@gmail.com>
2021-12-11 22:24:39 +0300

    remove import firebase from service js file

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,3 @@
-import firebase from "../src/firebase";
-
 importScripts("https://www.gstatic.com/firebasejs/7.13.2/firebase-app.js");
 importScripts(
   "https://www.gstatic.com/firebasejs/7.13.2/firebase-messaging.js"
