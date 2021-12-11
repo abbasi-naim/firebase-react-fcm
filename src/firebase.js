11e72528d2fd1752148c79ccdd7d498fa82a203c
abbasi-naim <abbasi.naim@gmail.com>
2021-12-11 20:58:18 +0300

    initialize firebase app

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 const config = {
   apiKey: "AIzaSyBbYcdcD-_9wvsaDcB_E2klugY0vFawsHA",
@@ -8,3 +8,7 @@ const config = {
   messagingSenderId: "766739444064",
   appId: "1:766739444064:web:51ee36a7acadeb7a5dee05",
 };
+
+firebase.initializeApp(config);
+
+export default firebase;
