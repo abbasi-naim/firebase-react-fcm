a10d7ab51603b22832fa8730328582ba44f7c6dc
abbasi-naim <abbasi.naim@gmail.com>
2021-12-11 21:27:56 +0300

    initializeApp in firebase service

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,2 +1,10 @@
-importScripts('https://www.gstatic.com/firebasejs/7.13.2/firebase-app.js')
-importScripts("https://www.gstatic.com/firebasejs/7.13.2/firebase-messaging.js");
+import firebase from "../src/firebase";
+
+importScripts("https://www.gstatic.com/firebasejs/7.13.2/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/7.13.2/firebase-messaging.js"
+);
+
+firebase.initializeApp({
+  messagingSenderId: "766739444064",
+});
