bf7d6c49e0d17a279321fe2744befc537d1b675a
abbasi-naim <abbasi.naim@gmail.com>
2021-12-11 20:55:42 +0300

    add firebase config keys and values

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,10 @@
 import firebase from 'firebase'
 
 const config = {
-    
-}
+  apiKey: "AIzaSyBbYcdcD-_9wvsaDcB_E2klugY0vFawsHA",
+  authDomain: "fir-react-fcm.firebaseapp.com",
+  projectId: "fir-react-fcm",
+  storageBucket: "fir-react-fcm.appspot.com",
+  messagingSenderId: "766739444064",
+  appId: "1:766739444064:web:51ee36a7acadeb7a5dee05",
+};
