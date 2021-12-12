249ef261ade76786bac334cec067679a11d24262
abbasi-naim <abbasi.naim@gmail.com>
2021-12-12 18:06:42 +0300

    Last commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import firebase from "./firebase";
 import "./App.css";
+import logo from "./firebase.png";
 
 export default class App extends Component {
   constructor(props) {
@@ -23,6 +24,12 @@ export default class App extends Component {
       });
   }
   render() {
-    return <h2>This is my first fcm notification</h2>;
+    return (
+      <div>
+        <h2>This is my first fcm notification</h2>;
+        <img src={logo} alt="Firebase Logo" />
+        <a href="https://www.linkedin.com/in/naim-abbasi-darab/">My Linkedin</a>
+      </div>
+    );
   }
 }
